a6d30cbb5cf2abe2c62ce8f39004cfeb6ea73fb0
Sibiryk03 <dima.kravtsov.2703@gmail.com>
2025-01-18 16:53:03 +0700

    new project

---
+++ src/test/java/Autotests/DuckActionTest.java
@@ -0,0 +1,14 @@
+package Autotests;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+public class DuckActionTest extends TestNGCitrusSpringSupport{
+
+}
